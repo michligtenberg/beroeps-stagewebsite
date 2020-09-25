31bcbbaad9f076db879bfe52ecdf1bd5f191153d
Thom Veldhuis <53975935+Thom2503@users.noreply.github.com>
2020-09-25 10:58:54 +0200

    login, student en mentor

---
+++ home_student.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Student</title>
+  </head>
+  <body>
+    <h1>Hallo ik ben student</h1>
+  </body>
+</html>
