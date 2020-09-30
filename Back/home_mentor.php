ea70050fb561afbb34c2dbff603927ff7af567ff
christmasjones7 <56556906+christmasjones7@users.noreply.github.com>
2020-09-30 09:10:12 +0200

    changed home_mentor.php
    hij gaf 2,8 procent hack aan in github en heb veranderd om te zien of er iets veranderd.

--- Back/home_mentor.php
+++ Back/home_mentor.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Mentor</title>
-  </head>
-  <body>
-    <h1>Hallo ik ben mentor</h1>
-  </body>
-</html>
+<?php
+echo "hello"
+?>
