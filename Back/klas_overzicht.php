2e7cc39b745ff73288136522b5f32417286ba311
Nikuś <71121709+nikvs216@users.noreply.github.com>
2020-10-05 10:13:55 +0200

    updated

--- Back/klas_overzicht.php
+++ Back/klas_overzicht.php
@@ -3,25 +3,32 @@
  ?>
 <?php
 require_once 'config.php';
+
 $Klas = $_SESSION['Klas'];
 //var_dump($Klas);
 $sql = "SELECT * FROM studenten WHERE Klas = '$Klas'";
 $result = mysqli_query($mysql, $sql);
 echo  "<table>";
 echo "<tr>";
+echo "<th>ID</th>";
 echo "<th>Naam</th>";
 echo "<th>Studentnummer</th>";
 
 echo "</tr>";
+
+//var_dump($id);
 if (!$result) {
   printf("Error: %s\n", mysqli_error($mysql));
   exit();
 }
+
 while ($row = mysqli_fetch_array($result)) {
     echo "<tr>";
-    echo "<td>" . $row['Naam'] . "</td>";
+    echo "<td>" . $row['StudentID'] . "</td>";
+    echo "<td><a href='student_details.php?id=" .$row['StudentID']. "'>" . $row['Naam'] . "</td></a>";
     echo "<td>" . $row['StudentNummer'] . "</td>";
     echo "</tr>";
+
 }
 echo "</table>"
 ?>
